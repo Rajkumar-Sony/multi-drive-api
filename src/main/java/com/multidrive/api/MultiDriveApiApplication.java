8b3b167c48b11d8ed3fe9ba488d29e965b3bc66b
Rajkumar-Sony <rk.sony9211@gmail.com>
2026-08-15 23:59:09 +0900

    Initialize multi-drive-api project with Maven setup and basic application structure

---
+++ src/main/java/com/multidrive/api/MultiDriveApiApplication.java
@@ -0,0 +1,13 @@
+package com.multidrive.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDriveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDriveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multidrive/api/MultiDriveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multidrive.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDriveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
